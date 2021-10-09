c7274630961007ed266bc2e26edb811041205d74
Souravendra Krishna Deb <souravendra.official@gmail.com>
2021-10-09 12:02:08 +0530

    updated tailwind config

--- sorting-visualizer/tailwind.config.js
+++ sorting-visualizer/tailwind.config.js
@@ -1,11 +1,38 @@
 module.exports = {
-  purge: [],
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    colors: {
+      gray: colors.coolGray,
+      blue: colors.lightBlue,
+      red: colors.rose,
+      pink: colors.fuchsia,
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      }
+    }
   },
   variants: {
-    extend: {},
+    fill: [],
+    extend: {
+      borderColor: ['focus-visible'],
+      opacity: ['disabled'],
+    }
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/typography'),
+    require('tailwindcss-children'),
+  ],
 }
